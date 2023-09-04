38554de55dacd1e116bedb4d4601649a78a30654
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-09-04 14:37:16 +0200

    adding routes to blogs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
+  components: { PostList },
+  setup() {
+    const { posts, error, load } = getPosts();
+
+    load();
+
+    return { posts, error };
+  },
 };
 </script>
